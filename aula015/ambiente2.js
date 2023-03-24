204e55935a8e141ecc2c966f3c182b3788ddd359
dan.vsm <dan.vsm@gmail.com>
2023-03-24 19:50:28 -0300

    att

---
+++ aula015/ambiente2.js
@@ -0,0 +1,25 @@
+/*
+let c = 0
+let pos = []
+
+do {
+    c++
+    pos[c] = c
+    console.log(`Contado: ${pos[c]}`)
+
+} while (c < 4) */
+
+/*let num = [4,5,6,7,8]
+for (let pos=0; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+} 
+
+let num = [1,2,3,4,5]
+
+for ( let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+*/
+
+let num = [4,5,6,7,8]
+console.log(num.indexOf(5))
